0e2b241278cc2b802d78824f91d07f95b13734e2
stojov <stojovaleksandar4@gmail.com>
2022-01-20 20:18:19 +0545

    Added handler to get all job

--- handler.py
+++ handler.py
@@ -8,7 +8,8 @@ def put_job(event, context):
     rssUrl = data['rssUrl']
     schedule = data['schedule']
 
-    dynamodb = boto3.resource('dynamodb', endpoint_url=os.environ.get('DB_URL'))
+    dynamodb = boto3.resource('dynamodb', access_key_id=os.environ.get('AWS_ACCESS_KEY'), secret_access_key=os.environ.get(
+        'AWS_SECRET_ACCESS_KEY'), aws_region=os.environ.get('AWS_REGION'), endpoint_url=os.environ.get('DB_URL'))
 
     table = dynamodb.Table('Jobs')
     response = table.put_item(
@@ -17,5 +18,17 @@ def put_job(event, context):
             'schedule': schedule,
         }
     )
+
+    return response
+
+
+def get_job(event, context):
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'), aws_secret_access_key=os.environ.get(
+        'AWS_SECRET_ACCESS_KEY'), region_name=os.environ.get('AWS_REGION'), endpoint_url=os.environ.get('DB_URL'))
+
+    table = dynamodb.Table('Jobs')
+    response = table.scan()
+
+    response = json.dumps(response)
 
     return response

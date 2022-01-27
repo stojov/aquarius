85e74366ada57695bd9bed99ad58eb65a5e3100c
stojov <stojovaleksandar4@gmail.com>
2022-01-27 21:41:49 +0545

    Add modified and etag to jobs table

--- create_table.py
+++ create_table.py
@@ -28,36 +28,6 @@ def create_jobs_table():
         print(e)
 
 
-def create_parse_record_table():
-    try:
-        table = dynamodb.create_table(
-            TableName='ParseRecords',
-            KeySchema=[
-                {
-                    'AttributeName': 'id',
-                    'KeyType': 'HASH'
-                },
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'id',
-                    'AttributeType': 'S'
-                },
-
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
-            }
-        )
-        return table
-    except Exception as e:
-        print(e)
-
-
 if __name__ == '__main__':
     job_table = create_jobs_table()
-    parse_record_table = create_parse_record_table()
     print("Table status:", job_table.table_status)
-    print("Table status:", parse_record_table.table_status)
-
--- handlers/url_parser.py
+++ handlers/url_parser.py
@@ -1,19 +1,52 @@
-import json
 import feedparser
+from helpers.db import dynamodb
 
 
 def rss_parser(event, context):
     try:
-        rssUrl = event['rssUrl']
+        id = event['queryStringParameters']['id']
+        table = dynamodb.Table('Jobs')
 
-        feed = feedparser.parse(rssUrl)
+        result = table.get_item(Key={'id': id})
+        job = result.get('Item')
+
+        etag = job.get('etag')
+        modified = job.get('modified')
+
+        feed = feedparser.parse(
+            job.get('rssUrl'), etag=etag, modified=modified)
 
         for item in feed.entries:
             print(item.title)
             print(item.published)
-        
-    
-    except e:
+
+        etag = feed.get('etag')
+        modified = feed.get('modified')
+
+        table.update_item(
+            Key={'id': id},
+            UpdateExpression="SET etag=:etag, modified=:modified",
+            ExpressionAttributeValues={
+                ':etag': etag,
+                ':modified': modified,
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": 'Ok'
+        }
+
+    except Exception as e:
         print(e)
-
-
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+            },
+            "body": 'Internal Server Error'
+        }
